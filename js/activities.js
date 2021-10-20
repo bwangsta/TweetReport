40f318ae6f2c2d1121e33aca8c1f947b0f136a06
Brian Wang <bwbrianwang1018@gmail.com>
2021-10-19 22:46:37 -0700

    Finished activities.js

--- js/activities.js
+++ js/activities.js
@@ -1,14 +1,4 @@
-function parseTweets(runkeeper_tweets) {
-    //Do not proceed if no tweets loaded
-    if (runkeeper_tweets === undefined) {
-        window.alert('No tweets returned');
-        return;
-    }
-
-    tweet_array = runkeeper_tweets.map(function (tweet) {
-        return new Tweet(tweet.text, tweet.created_at);
-    });
-
+function countActivityTypes(tweet_array) {
     let counts = {
         "ski run": 0,
         "run": 0,
@@ -20,6 +10,17 @@ function parseTweets(runkeeper_tweets) {
         "yoga": 0,
         "health graph": 0,
         "gym": 0,
+        "swim": 0,
+        "activity": 0,
+        "skate": 0,
+        "row": 0,
+        "chair ride": 0,
+        "freestyle": 0,
+        "pilates": 0,
+        "boxing": 0,
+        "sports": 0,
+        "snowboard": 0,
+        "dance": 0,
         "unknown": 0
     };
 
@@ -55,25 +56,223 @@ function parseTweets(runkeeper_tweets) {
             case "gym":
                 counts["gym"]++;
                 break;
-            default:
+            case "swim":
+                counts["swim"]++;
+                break;
+            case "activity":
+                counts["activity"]++;
+                break;
+            case "skate":
+                counts["skate"]++;
+                break;
+            case "row":
+                counts["row"]++;
+                break;
+            case "chair ride":
+                counts["chair ride"]++;
+                break;
+            case "freestyle":
+                counts["freestyle"]++;
+                break;
+            case "pilates":
+                counts["pilates"]++;
+                break;
+            case "boxing":
+                counts["boxing"]++;
+                break;
+            case "sports":
+                counts["sports"]++;
+                break;
+            case "snowboard":
+                counts["snowboard"]++;
+                break;
+            case "dance":
+                counts["dance"]++;
+                break;
+            case "unknown":
                 counts["unknown"]++;
         }
     });
 
+    return counts;
+}
+
+
+// gets the top 3 activities
+function top3Activities(activityCounts) {
+    const sortedCounts = Object.entries(activityCounts).sort(([, a], [, b]) => b - a);
+
+    return sortedCounts.slice(0, 3);
+}
+
+
+function createDistancesArray(tweet_array, top3) {
+    let distancesArray = []
+    tweet_array.forEach((tweet) => {
+        distancesObject = {};
+        switch (tweet.activityType) {
+            case top3[0][0]:
+            case top3[1][0]:
+            case top3[2][0]:
+                distancesObject["activity_type"] = tweet.activityType;
+                distancesObject["distance"] = tweet.distance
+                distancesObject["day"] = tweet.time.toLocaleString("en-US", { weekday: "short" });
+                distancesArray.push(distancesObject);
+                break;
+        }
+    });
+
+    return distancesArray;
+}
+
+
+function findMaxMin(distancesArray) {
+    let maxObj = { "type": "", "distance": Number.NEGATIVE_INFINITY };
+    let minObj = { "type": "", "distance": Number.POSITIVE_INFINITY };
+
+    distancesArray.forEach((distances) => {
+        if (distances["distance"] > maxObj["distance"]) {
+            maxObj = distances;
+        }
+        else if (distances["distance"] < minObj["distance"] && distances["distance"] !== 0) {
+            minObj = distances;
+        }
+    });
+
+    return [minObj, maxObj];
+}
+
+
+function longestActivityDay(tweet_array, type) {
+    let weekdayCount = 0;
+    let weekendCount = 0;
+    tweet_array.forEach((tweet) => {
+        if (tweet.activityType === type) {
+            if (tweet.typeOfDay === "weekend") {
+                weekendCount++;
+            }
+            else {
+                weekdayCount++;
+            }
+        }
+    });
+
+    return weekdayCount > weekendCount ? "weekdays" : "weekends";
+}
+
+
+function parseTweets(runkeeper_tweets) {
+    //Do not proceed if no tweets loaded
+    if (runkeeper_tweets === undefined) {
+        window.alert('No tweets returned');
+        return;
+    }
+
+    tweet_array = runkeeper_tweets.map(function (tweet) {
+        return new Tweet(tweet.text, tweet.created_at);
+    });
+
+    let counts = countActivityTypes(tweet_array);
+    document.querySelector("#numberActivities").textContent = Object.keys(counts).length - 1;
+
+    let top3 = top3Activities(counts);
+    document.querySelector("#firstMost").textContent = top3[0][0];
+    document.querySelector("#secondMost").textContent = top3[1][0];
+    document.querySelector("#thirdMost").textContent = top3[2][0];
+
+    let distancesArray = createDistancesArray(tweet_array, top3);
+    console.log(distancesArray);
+    let minMaxArray = findMaxMin(distancesArray);
+
+    document.querySelector("#shortestActivityType").textContent = minMaxArray[0]["activity_type"];
+    document.querySelector("#longestActivityType").textContent = minMaxArray[1]["activity_type"];
+
+    let day = longestActivityDay(tweet_array, minMaxArray[1]["activity_type"]);
+    document.querySelector("#weekdayOrWeekendLonger").textContent = day;
+
+
     //TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
+    let activities_array = [];
+    tweet_array.forEach((tweet) => {
+        if (tweet.source === "completed_event") {
+            activities_array.push({ "activity_type": tweet.activityType });
+        }
+    });
 
     activity_vis_spec = {
         "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
         "description": "A graph of the number of Tweets containing each type of activity.",
         "data": {
-            "values": tweet_array
-        }
+            "values": activities_array
+        },
         //TODO: Add mark and encoding
+        "mark": "bar",
+        "encoding": {
+            "x": { "field": "activity_type", "type": "nominal", "title": "Types of Activities" },
+            "y": { "aggregate": "count", "field": "activity_type", "type": "quantitative", "title": "Count" }
+        }
     };
+
+    distance_vis_spec = {
+        "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+        "description": "A graph of the number of Tweets containing each type of activity.",
+        "data": {
+            "values": distancesArray
+        },
+        //TODO: Add mark and encoding
+        "mark": "point",
+        "encoding": {
+            "x": { "field": "day", "type": "ordinal", "title": "Time (day)", "sort": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] },
+            "y": { "field": "distance", "type": "quantitative" },
+            "color": { "field": "activity_type" }
+        }
+    };
+
+    mean_distance_vis_spec = {
+        "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+        "description": "A graph of the number of Tweets containing each type of activity.",
+        "data": {
+            "values": distancesArray
+        },
+        //TODO: Add mark and encoding
+        "mark": "point",
+        "encoding": {
+            "x": { "field": "day", "type": "ordinal", "title": "Time (day)", "sort": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"] },
+            "y": { "field": "distance", "type": "quantitative", "aggregate": "mean" },
+            "color": { "field": "activity_type" }
+        }
+    };
+
+
     vegaEmbed('#activityVis', activity_vis_spec, { actions: false });
+    vegaEmbed('#distanceVis', distance_vis_spec, { actions: false });
+    vegaEmbed('#distanceVisAggregated', mean_distance_vis_spec, { actions: false });
 
     //TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
     //Use those visualizations to answer the questions about which activities tended to be longest and when.
+
+    let btn = document.querySelector("#aggregate");
+    let distanceGraph = document.querySelector("#distanceVis");
+    let meanDistanceGraph = document.querySelector("#distanceVisAggregated");
+    distanceGraph.style.display = "none";
+    meanDistanceGraph.style.display = "none";
+
+    btn.addEventListener("click", function () {
+        if (distanceGraph.style.display === "none" && meanDistanceGraph.style.display === "none") {
+            distanceGraph.style.display = "block";
+            btn.textContent = "Show All Activities";
+        }
+        else if (distanceGraph.style.display === "block") {
+            distanceGraph.style.display = "none";
+            meanDistanceGraph.style.display = "block";
+            btn.textContent = "Show means";
+        }
+        else {
+            distanceGraph.style.display = "block";
+            meanDistanceGraph.style.display = "none";
+            btn.textContent = "Show All Activities";
+        }
+    })
 }
 
 //Wait for the DOM to load
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -10,7 +10,7 @@ class Tweet {
     //returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source(): string {
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        if (this.text.toLowerCase().includes("completed") || this.text.toLowerCase().includes("just")) {
+        if (this.text.toLowerCase().includes("just")) {
             return "completed_event";
         }
         else if (this.text.toLowerCase().includes("live")) {
@@ -42,50 +42,129 @@ class Tweet {
     }
 
     get activityType(): string {
-        if (this.source != 'completed_event') {
-            return "unknown";
+        if (this.source === 'completed_event') {
+            if (this.text.toLowerCase().includes("ski run")) {
+                return "ski run";
+            }
+            else if (this.text.toLowerCase().includes(" run") || (this.text.toLowerCase().includes("ran"))) {
+                return "run";
+            }
+            else if (this.text.toLowerCase().includes("walk")) {
+                return "walk";
+            }
+            else if (this.text.toLowerCase().includes("bike")) {
+                return "bike";
+            }
+            else if (this.text.toLowerCase().includes("hike")) {
+                return "hike";
+            }
+            else if (this.text.toLowerCase().includes("workout")) {
+                return "workout";
+            }
+            else if (this.text.toLowerCase().includes("meditation")) {
+                return "meditation";
+            }
+            else if (this.text.toLowerCase().includes("yoga")) {
+                return "yoga";
+            }
+            else if (this.text.toLowerCase().includes("health graph")) {
+                return "health graph";
+            }
+            else if (this.text.toLowerCase().includes("gym")) {
+                return "gym";
+            }
+            else if (this.text.toLowerCase().includes("swim")) {
+                return "swim";
+            }
+            else if (this.text.toLowerCase().includes("activity")) {
+                return "activity";
+            }
+            else if (this.text.toLowerCase().includes("skate")) {
+                return "skate";
+            }
+            else if (this.text.toLowerCase().includes("row")) {
+                return "row";
+            }
+            else if (this.text.toLowerCase().includes("chair ride")) {
+                return "chair ride";
+            }
+            else if (this.text.toLowerCase().includes("freestyle")) {
+                return "free style";
+            }
+            else if (this.text.toLowerCase().includes("pilates")) {
+                return "pilates";
+            }
+            else if (this.text.toLowerCase().includes("boxing")) {
+                return "boxing";
+            }
+            else if (this.text.toLowerCase().includes("sports")) {
+                return "sports";
+            }
+            else if (this.text.toLowerCase().includes("snowboard")) {
+                return "snowboard";
+            }
+            else if (this.text.toLowerCase().includes("dance")) {
+                return "dance";
+            }
         }
 
-        if (this.text.toLowerCase().includes("ski run")) {
-            return "ski run";
-        }
-        else if (this.text.toLowerCase().includes(" run")) {
-            return "run";
-        }
-        else if (this.text.toLowerCase().includes("walk")) {
-            return "walk";
-        }
-        else if (this.text.toLowerCase().includes("bike")) {
-            return "bike";
-        }
-        else if (this.text.toLowerCase().includes("hike")) {
-            return "hike";
-        }
-        else if (this.text.toLowerCase().includes("workout")) {
-            return "workout";
-        }
-        else if (this.text.toLowerCase().includes("meditation")) {
-            return "meditation";
-        }
-        else if (this.text.toLowerCase().includes("yoga")) {
-            return "yoga";
-        }
-        else if (this.text.toLowerCase().includes("health graph")) {
-            return "health graph";
-        }
-        else if (this.text.toLowerCase().includes("gym")) {
-            return "gym";
-        }
         //TODO: parse the activity type from the text of the tweet
-        return "";
+        return "unknown";
     }
 
     get distance(): number {
-        if (this.source != 'completed_event') {
-            return 0;
+        if (this.source === 'completed_event') {
+            //TODO: parse the distance from the text of the tweet
+            const regex = /\d+\.\d{2}/g;
+            let dist = this.text.match(regex);
+
+            if (dist !== null) {
+                if (dist[0] !== null) {
+                    if (this.text.includes(" km ")) {
+                        let miDist = parseFloat(dist[0]) / 1.609;
+                        return miDist;
+                    }
+                    else {
+                        return parseFloat(dist[0]);
+                    }
+                }
+            }
         }
-        //TODO: parse the distance from the text of the tweet
+
         return 0;
+    }
+
+    get typeOfDay(): string {
+        let day = this.time.getDay();
+
+        if (day === 0 || day === 6) {
+            return "weekend";
+        }
+
+        return "weekday";
+    }
+
+    get getDayName(): string {
+        let day = "";
+
+        switch (this.time.getDay()) {
+            case 0:
+                day = "Sunday";
+            case 1:
+                day = "Monday";
+            case 2:
+                day = "Tuesday";
+            case 3:
+                day = "Wednesday";
+            case 4:
+                day = "Thursday";
+            case 5:
+                day = "Friday";
+            case 6:
+                day = "Saturday";
+        }
+
+        return day;
     }
 
     getHTMLTableRow(rowNumber: number): string {
